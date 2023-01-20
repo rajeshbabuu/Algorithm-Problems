07d95d46cd653d2d73af44cfe682e6d4a800f361
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-20 11:50:59 +0530

    Permutations of a string

---
+++ AlgorithmPrograms/PermutationOfString.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class PermutationOfString
+    {
+        // Iterative Method
+        public static void Iterative(String word, String answer)
+        {
+            if (word.Length == 0)
+            {
+                Console.WriteLine(answer + " ");
+                return;
+            }
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char ch = word[i];
+                String left_substr = word.Substring(0, i);
+                String right_substr = word.Substring(i + 1);
+                String rest = left_substr + right_substr;
+                Iterative(rest, answer + ch);
+            }
+        }
+
+
+        // Recursion Method
+        public static void Recursion(String word, int initialIndex, int endIndex)
+        {
+            if (initialIndex == endIndex)
+            {
+                Console.WriteLine(word);
+            }
+            else
+            {
+                for (int i = initialIndex; i <= endIndex; i++)
+                {
+                    word = Swap(word, initialIndex, i);
+                    Recursion(word, initialIndex + 1, endIndex);
+                    word = Swap(word, initialIndex, i);
+                }
+            }
+        }
+        public static string Swap(String word, int pos1, int pos2)
+        {
+            char temp;
+            char[] charArray = word.ToCharArray();
+            temp = charArray[pos1];
+            charArray[pos1] = charArray[pos2];
+            charArray[pos2] = temp;
+            string swapped = new string(charArray);
+            return swapped;
+        }
+    }
+}
---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+
+            Console.WriteLine("Enter the string : ");
+            string word = Console.ReadLine();
+            int n = word.Length;
+            string answer = " ";
+
+            Console.WriteLine("\nAll possible strings are : ");
+
+            Console.WriteLine("\nIterative Method: ");
+            PermutationOfString.Iterative(word, answer);
+
+            Console.WriteLine("\nRecursive Method: ");
+            PermutationOfString.Recursion(word, 0, n - 1);
+        }
+    }
+}
